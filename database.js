dbeffa1c641b592e0fc7f8b4779af51f09eba5cf
Riccardo Berta <riccardo.berta@gmail.com>
2020-01-13 17:38:53 +0100

    management of missing DB connection at startup

--- database.js
+++ database.js
@@ -8,15 +8,34 @@ mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 
+const sleep = function(ms){
+    return new Promise(resolve=>{
+        setTimeout(resolve, ms)
+    })
+}
+
+const connection = async function(){
+    let go = false;
+    while(!go) {
+        try { 
+            await mongoose.connect(process.env.DATABASE); 
+            console.error('Database connected!')
+            go = true;
+        } 
+        catch (error) { 
+            console.error('Database connection error, retry in 3 secs...'); 
+            await sleep(3000);
+        }
+    } 
+}
+
 if(process.env.ENV === "test") {
     const Mockgoose = new require('mockgoose').Mockgoose;
     const mockgoose = new Mockgoose(mongoose);
     mockgoose.prepareStorage().then(function() { mongoose.connect(process.env.DATABASE); });
 }
-else { mongoose.connect(process.env.DATABASE); }
-
-mongoose.connection.on('error', (err) => { console.error('Database connection error '+ err.message); });
-
+else { connection() }
+ 
 mongoosePaginate.paginate.options = { lean: false };
 
 require('./models/userSchema');
