1193c3d2aa8ca7a80f4b4048a3673258379bee89
Riccardo Berta <riccardo.berta@gmail.com>
2020-02-03 10:11:03 +0100

    added more values to alert types

--- types/alertTypes.js
+++ types/alertTypes.js
@@ -1,6 +1,10 @@
 const AlertTypes = Object.freeze({
     generic: 'generic', 
-    hardware_issue: 'hardware_issue'
+    hardware_issue: 'hardware_issue',
+    initialization_issue: 'initialization_issue',
+    field_issue: 'field_issue',
+    script_issue: 'script_issue'
+
 });
 
 module.exports = AlertTypes;
