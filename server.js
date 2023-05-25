f52dcad86f141424b5e3268495f2af79604baabd
mattefresta <matteofresta97@gmail.com>
2023-05-25 14:24:17 +0200

    Added support for more certificates

--- server.js
+++ server.js
@@ -28,13 +28,13 @@ app.set('view engine', 'pug');
 
 // CORS (cross-domain requests)
 app.use(cors());
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     next();
 });
 
 // Logger
-if(process.env.LOG === 'true') {
+if (process.env.LOG === 'true') {
     const logger = require('./commons/logger.js');
     app.use(logger);
 }
@@ -47,20 +47,55 @@ app.get('/' + process.env.VERSION, (req, res, next) => { res.redirect(req.baseUr
 
 // Provide API version information
 const { version } = require('./package.json');
-app.get('/' + process.env.VERSION + '/version', (req, res, next) => { return res.status(200).json({ version: version}); });
+app.get('/' + process.env.VERSION + '/version', (req, res, next) => { return res.status(200).json({ version: version }); });
 
 // Takes the raw requests and turns them into usable properties on req.body
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // Create HTTP/HTTPS server
 let server = null;
 let port = process.env.HTTP_PORT;
 let message = 'Measurify Cloud API Server is running on HTTP';
-if (process.env.PROTOCOL === 'http') {  server = http.createServer(app); }
+if (process.env.PROTOCOL === 'http') { server = http.createServer(app); }
 else {
     try {
-        const config = { key: fs.readFileSync(key_file_prod), cert: fs.readFileSync(cert_file_prod), passphrase: process.env.HTTPSSECRET };
+        //function to pick out the key + certs dynamically based on the domain name
+        function createSecureContext() {
+            var secureContext = {}
+            fs.readdirSync('./resources/').forEach(el => {
+                if (!el.endsWith(".pem")) {
+                    secureContext[el] = tls.createSecureContext({
+                        key: fs.readFileSync('./resources/'+el+'/privkey.pem', 'utf8'),
+                        cert: fs.readFileSync('./resources/'+el+'/fullchain.pem', 'utf8'),
+                        passphrase: process.env.HTTPSSECRET                        
+                    })
+                }
+            })
+            return secureContext;
+        }
+
+        var secureContext = createSecureContext()
+
+        //provide a SNICallback when you create the options for the https server
+        var config = {
+            SNICallback: function (domain, cb) {
+                if (secureContext[domain]) {
+                    if (cb) {
+                        cb(null, secureContext[domain]);
+                    } else {
+                        // compatibility for older versions of node
+                        return secureContext[domain];
+                    }
+                } else {
+                    throw new Error('No keys/certificates for domain requested');
+                }
+            },
+            // must list a default key and cert because required by tls.createServer()
+            key: fs.readFileSync(key_file_prod),
+            cert: fs.readFileSync(cert_file_prod), 
+            passphrase: process.env.HTTPSSECRET
+        }
         server = https.createServer(config, app);
         port = process.env.HTTPS_PORT;
         message = 'Measurify Cloud API Server is running on HTTPS';
@@ -72,7 +107,8 @@ else {
             port = process.env.HTTPS_PORT;
             message = 'WARNING: Measurify Cloud API Server is running on HTTPS with self signed certificate (' + error + ')';
         }
-        catch(error) { server = http.createServer(app);
+        catch (error) {
+            server = http.createServer(app);
             port = process.env.HTTP_PORT;
             message = 'WARNING: HTTPS not running (' + error + '), Measurify Cloud API Server is running on HTTP';
         }
