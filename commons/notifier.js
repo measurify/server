5803d56f91cc864a07529da7f6efbc34fa85aec4
mattefresta <matteofresta97@gmail.com>
2023-12-13 16:29:17 +0100

    .

--- commons/notifier.js
+++ commons/notifier.js
@@ -23,7 +23,7 @@ exports.send = async function(token, title, body) {
                  multicastId: 7504248744698765000
                 }
     }
-    const payload = { notification : { title: "Avviso qualità dell'aria", body: "Nuovi dati disponibili. Clicca per visualizzarli" } , data: { "body": body} };
+    const payload = { notification : { title: "Avviso qualità dell'aria", body: "Nuovi dati disponibili. Clicca per visualizzarli" } , data: { body: body} };
     //const payload = { notification: { title: title, body: body } };
     const res = await firebase.messaging().sendToDevice(token, payload, options);
     
